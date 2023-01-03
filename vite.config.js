4d033f16b4704ae39246a503622b6d513f9a9edb
Ivan <ivan.inclan.327b@gmail.com>
2023-01-03 03:18:05 -0600

    se agrego la configuracion para ghpages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://1vanInclan.github.io/diario-app/",
   plugins: [react()]
 })
